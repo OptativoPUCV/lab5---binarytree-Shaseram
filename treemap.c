e46d65d71deadb666736dcf175c007baac71dfa9
Replit user <>
2023-04-22 00:13:55 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -201,7 +201,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
     }
   }
   if (ub_node == NULL) return NULL;
-  if (tree->lower_than(ub_node->pair->key,key) == 1) return NULL;
+  if (tree->lower_than(key,ub_node->pair->key) == 1) return NULL;
   
   
   return ub_node->pair;
